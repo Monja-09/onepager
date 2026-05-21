323b78bcc2644c7e9fff950abadb93feb7972da8
Monja-09 <monja.tanner@bzwu.ch>
2026-05-21 18:06:59 +0200

    JavaScript for Countdown

---
+++ script.js
@@ -0,0 +1,34 @@
+// Datum des Match (24. Mai 2026 um 14:00 Uhr)
+const matchDate = new Date("May 24, 2026 14:00:00").getTime();
+
+const countdownTimer = setInterval(function() {
+
+    //aktuelle Zeit abrufen
+    const now = new Date().getTime();
+
+    const distance = matchDate - now;
+
+    //wenn Countdown fertig
+    if (distance <= 0) {
+        clearInterval(countdownTimer); 
+        document.getElementById("tage").innerHTML = "00";
+        document.getElementById("stunden").innerHTML = "00";
+        document.getElementById("minuten").innerHTML = "00";
+        document.getElementById("sekunden").innerHTML = "00";
+        return; 
+    }
+    
+    //zeitberechnungen
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    //werte einfügen
+    document.getElementById("tage").innerHTML = days.toString().padStart(2, '0');
+    document.getElementById("stunden").innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById("minuten").innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById("sekunden").innerHTML = seconds.toString().padStart(2, '0');
+    
+
+}, 1000);
